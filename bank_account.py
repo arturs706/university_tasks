cc456fce75482a1253a6b0cc72fb133ea5bf1bcc
Arturs <arturs@Arturss-Air.broadband>
2022-04-14 22:58:18 +0100

    first commit

--- bank_account.py
+++ bank_account.py
@@ -7,26 +7,29 @@ class BankAccount:
     def print_balance(self): 
         print(f"Account balance for {self.account_holder}'s account {self.account_number} is: {str(self.balance)}")
 
-    def print_name(self):
-        print(f"Account holder for account nr: {self.account_number} is: {self.account_holder}")
+
+    def withdraw (self,with_d):
+        print (f'\n{self.account_holder} is withdrawing £{with_d} from the account.\nThe new balance is as follows:')
+        self.balance = self.balance - with_d
+        
+
+    def deposit (self,depo):
+        print (f'\n{self.account_holder} is depositing £{depo} to the account.\nThe new balance is as follows:')
+        self.balance = self.balance + depo
 
 
 
-
-account1 = BankAccount('Alice',"123456789", 1000.00) 
+account1 = BankAccount('Alice',"123456789", 10000.00) 
 account2 = BankAccount('Bob','9955233222', 95688.28)
 account3 = BankAccount('Jane','9856333255', -885533.32)
 account4 = BankAccount('George','1112399999', 9909933.32)
 
-print ('Creating four bank account objects and calling print_balance function\n')
+
 account1.print_balance()
 account2.print_balance()
 account3.print_balance()
 account4.print_balance()
-
-print ('Calling the print_name function\n')
-
-account1.print_name()
-account2.print_name()
-account3.print_name()
-account4.print_name()
+account1.withdraw(100)
+account1.print_balance()
+account2.deposit(200)
+account2.print_balance()
